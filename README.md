aa0a031a531f8cae6c28876bda333dfeef52ce0e
Brian Provenzano <bproven@gmail.com>
2018-04-14 00:50:39 -0700

    refactor: refactored messaging, processing functions, added quick and dirty console dump of histogram

--- README.md
+++ README.md
@@ -4,40 +4,51 @@ Create Weather Histogram
 ## Description
 Creates a tsv file of histogram bins of weather forecast based on logfile input (containing IP addresses) and requested bin size
 
+## Usage
+```
+create-weather-hist.py <input-logfilename> <output-filename> <hist-buckets>
+```
+
 ## Initial Setup
-You will need the following third party modules, just pip3 install them as needed:
+You will need the following third party modules, just pip3 install them as needed or use the requirements.txt file as noted below:
 - requests (http://docs.python-requests.org/en/master/)
-- geoip2.database (https://github.com/maxmind/GeoIP2-python)
-- python 3 (tested on python 3.6.5 on Linux)
+- geoip2(https://github.com/maxmind/GeoIP2-python)
+- numpy (https://www.scipy.org/scipylib/download.html , http://www.numpy.org/)
+
+Example pip3 install the requirements:
+```
+pip3 install -r requirements.txt
+
+```
+
+Note: Tested and developed on python 3.6.5 on Linux (Fedora)
 
 ### The following settings must be manually preconfigured in 'create-weather-histogram.py'
 
-Under 'src' directory you will find the compressed GeoLite2 city database, decompress into the files directory and
-change the reference below if needed
+1 Under 'src' directory you will find the compressed GeoLite2 city database, decompress into the files directory and change the reference below as needed
 ```
 GEOIP2_DB = "files/GeoLite2-City-20180403.mmdb"
 ```
 
-Go to https://openweathermap.org/api and obtain a free tier API key (note the limitations; this code should respect them) 
-Fill in your API key here:
+Go to https://openweathermap.org/api and obtain a free tier API key (note the limitations; this code should respect them).  Fill in your API key here:
 ```
 OPENWEATHER_APIKEY = "" #appid - free tier
 ```
 
-There are also a few other settings (constants) you may wish to set those are in the following section in the code:
+There are also a few other settings (constants) you may wish to set those are in the following section in the code in 'create-weather-histogram':
 ```
 ##########################################
 #- Modify the options below as needed
 ##########################################
 
- ...
+ ...settings here...
 
 ##########################################
 #- END - Do not modify below here!!!
 ##########################################
 ```
 
-##### NOTE: This code was tested on Fedora Linux (python 3.6.5) - there are no guarantees it will work on other platforms YMMV
+##### NOTE: This code was tested on Fedora Linux (python 3.6.5) - there are no guarantees it will work on other platforms.  It should but YMMV.
 
 ## Third Party APIs / Libraries Used
 
@@ -51,5 +62,7 @@ There are also a few other settings (constants) you may wish to set those are in
 - API limits and tiers: https://openweathermap.org/price
 
 ## Thanks
+
 - requests (http://docs.python-requests.org/en/master/)
 - geoip2.database (https://github.com/maxmind/GeoIP2-python)
+- NumPy (https://www.scipy.org/scipylib/download.html , http://www.numpy.org/)
--- create-weather-histogram.py
+++ create-weather-histogram.py
@@ -18,7 +18,7 @@ Third party APIs (or databases) used
 
 Usage:
 -----------------------
-create-weather-hist.py <input-logfilename> <output-filename> <number-bins>
+create-weather-hist.py <input-logfilename> <output-filename> <hist-buckets>
 
 BJP - 4/12/18
 """
@@ -26,9 +26,8 @@ BJP - 4/12/18
 #--3rd party - see readme for pip install
 import requests
 import geoip2.database
-#import numpy
-#import matplotlib.mlab as mlab
-#import matplotlib.pyplot as plt
+import numpy
+
 #--std mod/libs
 import time , datetime
 import json
@@ -47,7 +46,7 @@ from enum import Enum
 GEOIP2_DB = "files/GeoLite2-City-20180403.mmdb"
 #-Weather APIs
 OPENWEATHER_BASEURL = "http://api.openweathermap.org/data/2.5/forecast"
-OPENWEATHER_APIKEY = "a666357bdc452dd7a0527706873fe73c" #appid - free tier
+OPENWEATHER_APIKEY = "" #appid - free tier
 OPENWEATHER_UNITS = "imperial" #Options: imperial, standard, metric
 #These are passed to be safe on rate limits (number of calls allowed per minute by free tier is 60)
 OPENWEATHER_RATE_SLEEP_SECONDS = 1 #seconds to wait between calls to be nice (docs warn on velocity)
@@ -70,8 +69,8 @@ def Main():
             help="Log source file containing locations to find weather forecast data for")
     parser.add_argument("outputfile", type=str, \
             help="Name of the file to output the histogram data to in tsv format")
-    parser.add_argument("bins", type=str, \
-            help="Number of bins (bin size) for histogram")
+    parser.add_argument("buckets", type=int, \
+            help="Number of buckets for histogram")
     #-informational args
     parser.add_argument("-d", "--debug", action="store_true", 
             help="Debug mode - show more informational messages for debugging")
@@ -81,49 +80,28 @@ def Main():
     args = parser.parse_args()
 
     logfile = args.logfile.strip()
-    outputFileName = args.outputfile.strip()
-    numberOfHistogramBins = args.bins.strip()
+    outputFile = args.outputfile.strip()
+    numberBuckets = args.buckets
 
     try:
-        logParser = ParseLogFile(logfile)
-        ipAddressList = logParser[0]
-        #print(','.join(ipAddressList))
-        importedLinesCount = logParser[1]
-        importedLinesFailures = logParser[2]
-        importedLinesAttempted = logParser[3]
-        printSummary(ipAddressList,importedLinesAttempted,importedLinesFailures,
-            "Log file IPs","Processing IPs From Local Log File",args.debug)
-
-        locationParser = getLocations(ipAddressList)
-        locationsList = locationParser[0]
-        locationsCount = locationParser[1]
-        locationsFailures = locationParser[2]
-        locationsAttempted = locationParser[3]
-        printSummary(locationsList,locationsAttempted,locationsFailures,
-            "Locations Lookup DB", "Looking Up Locations In Local Location DB",args.debug)
-
-        weatherForecastParser = getWeatherForecast(locationsList,locationsCount)
-        weatherForecastList = weatherForecastParser[0]
-        weatherForecastFailures = weatherForecastParser[1]
-        weatherForecastAttempted = weatherForecastParser[2]
-        printSummary(weatherForecastList,weatherForecastAttempted,weatherForecastFailures,
-            "Forecast High Temperature","Obtaining Next Day Forecast High Temperatures From Web Service",args.debug)
-
-        createHistogram(outputFileName,numberOfHistogramBins,args)
+        ipAddressList = ParseLogFile(logfile,args)
+        locationsList = GetLocations(ipAddressList,args)
+        weatherForecastList = GetWeatherForecast(locationsList,args)
+        CreateHistogram(weatherForecastList,outputFile,numberBuckets,args)
 
     except ValueError as ve:
-        printMessage(MessageType.ERROR, "System ValueError occurred!",str(ve))
+        PrintMessage(MessageType.ERROR, "System ValueError occurred!",str(ve))
     except FileNotFoundError as fe:
-        printMessage(MessageType.ERROR, "The file not found! "
+        PrintMessage(MessageType.ERROR, "The file not found! "
             "Check to make sure you provided the logfile and/or the log file is in your path",str(fe))
     except Exception as e:
-        printMessage(MessageType.ERROR, "UNKNOWN Error Occurred!",str(e))
+        PrintMessage(MessageType.ERROR, "UNKNOWN Error Occurred!",str(e))
 
 
-def ParseLogFile(logFile):
+def ParseLogFile(logFile, argparse):
     """ 
     Parse the source log file for IP Addresses to use
-    Returns: ipAddresses, ipAddressCount, ipAddressFailures, ipAddressAttempts
+    Returns: ipAddresses
     """
     timer = SimpleTimer()
     with open(logFile, "r") as f:
@@ -139,25 +117,27 @@ def ParseLogFile(logFile):
                         socket.inet_aton(field)
                     except socket.error:
                         failures += 1
-                        printMessage(MessageType.ERROR,
+                        PrintMessage(MessageType.ERROR,
                             "Invalid IP Address found at line [ {0} ]!  Skipping line... "\
                             "Please double check the logfile!".format(linecount))
 
                     ipAddresses.append(field)
 
-            ipAddressCount = len(ipAddresses)       
+            # ipAddressCount = len(ipAddresses)
             ipAddressFailures = failures
             ipAddressAttempts = len(ipList)
 
     timer.stop()
-    timer.printSummary("getting IPs from local logfile")
-    return ipAddresses, ipAddressCount, ipAddressFailures, ipAddressAttempts
+    PrintSummary(ipAddresses,ipAddressAttempts,ipAddressFailures, timer.PrintSummary("getting IPs from local logfile"),
+            "Log file IPs","Processing IPs From Local Log File",argparse.debug)
+
+    return ipAddresses
 
 
-def getLocations(ipAddressList):
+def GetLocations(ipAddressList, argparse):
     """ 
     Get locations from list of IPs using geoip2 database - lat and long  
-    Returns: locations, locationsCount, locationsFailures, locationsAttempted
+    Returns: locations, locationsCount
     """
     timer = SimpleTimer()
     locations = []
@@ -170,34 +150,35 @@ def getLocations(ipAddressList):
                 locations.append(loc)
             except:
                 print()
-                printMessage(MessageType.ERROR,
+                PrintMessage(MessageType.ERROR,
                 "IP Address [{0}] not found in database!  Skipping location...!".format(item))
                 failures += 1
 
     locationsFailures = failures
-    locationsCount = len(locations)
     locationsAttempted = len(ipAddressList)
 
     timer.stop()
-    timer.printSummary("getting locations from local db")
-    return locations, locationsCount, locationsFailures, locationsAttempted
+    PrintSummary(locations,locationsAttempted,locationsFailures,timer.PrintSummary("getting locations from local db"),
+        "Locations Lookup DB", "Looking Up Locations In Local Location DB",argparse.debug)
+
+    return locations
 
 
-def getWeatherForecast(locationsList,locationsCount):
+def GetWeatherForecast(locationsList,argparse):
     """ 
     Get Weather forecast for next day using openweathermap API  
-    Returns: forecastTempList, forecastFailures, forecastAttempts
+    Returns: forecastList
     """
-
-    printMessage(MessageType.INFO,
-        "Getting weather forecast from openweather API for [{0}] locations..."\
-        "This will take approximately [{1}]".format(locationsCount, timeFromFloat(locationsCount* 1.5)))
+    locationsCount = len(locationsList)
+    PrintMessage(MessageType.INFO,
+        "Getting forecast info from service for [{0}] locations. "\
+        "This will take approximately [{1}]".format(locationsCount, TimeFromFloat(locationsCount* 1.5)))
 
     if (locationsCount > 500):
-        printMessage(MessageType.INFO,"NOTE: This is a large dataset!  You might want to go get a cup of coffee...")
+        PrintMessage(MessageType.INFO,"NOTE: This is a large dataset!  You might want to go get a cup of coffee...")
     
     timer = SimpleTimer()
-    forecastTempList = []
+    forecastList = []
     failures = 0
     for count,item in enumerate(locationsList, start=1):
         payload = {"appid":OPENWEATHER_APIKEY,"lat":item[0],"lon":item[1],"units":OPENWEATHER_UNITS}
@@ -206,8 +187,8 @@ def getWeatherForecast(locationsList,locationsCount):
             response.raise_for_status()
             if response.status_code == 200:
                 jsonResult = response.json()
-                forecastTempList.append(jsonResult["list"][0]["main"]["temp_max"])
-                printProgress(locationsCount,count,timer.getElapsed())
+                forecastList.append(jsonResult["list"][0]["main"]["temp_max"])
+                PrintProgress(locationsCount,count,timer.getElapsed())
             else:
                 failures += 1
                 raise requests.ConnectionError("Server did not return status 200 - returned [{0}]".format(response.status_code))
@@ -215,7 +196,7 @@ def getWeatherForecast(locationsList,locationsCount):
         #throw the base exp so we can continue on any error from requests
         except requests.exceptions.RequestException as re:
             failures += 1
-            printMessage(MessageType.ERROR,
+            PrintMessage(MessageType.ERROR,
             "Trying to obtain forecast for location : Timeout / httperror waiting for server connection / response", re)
 
         #stay within rate - not perfect, but should be ok for this...
@@ -225,19 +206,35 @@ def getWeatherForecast(locationsList,locationsCount):
     forecastAttempts = (locationsCount)
 
     timer.stop()
-    timer.printSummary("getting weather forecast data")
-    return forecastTempList, forecastFailures, forecastAttempts
+    PrintSummary(forecastList,forecastAttempts,forecastFailures, timer.PrintSummary("getting weather forecast data"),
+        "Forecast High Temperature","Obtaining Next Day Forecast High Temperatures From Web Service", argparse.debug)
+
+    return forecastList
 
 
-def createHistogram(outputFile,bins,argsparse):
+def CreateHistogram(forecastData,outputFile,buckets,argsparse):
     """ 
-    Create histogram bin file (tsv)  
+    Create histogram bin file (tsv)
     Returns: file and prints to console (option)
     """
     #TODO - print results to console and output (use quiet mode if asked) to tsv file
-    print("-------------------------------------")
-    print("--------- HISTOGRAM HERE STUB--------")
-    print("-------------------------------------")
+    # print("-------------------------------------")
+    # print("--------- HISTOGRAM HERE STUB--------")
+    # print("-------------------------------------")
+    # data = [81.18, 74.43, 69.75, 69.66, 42.04, 61.59, 35.42, 68.95, 65.52, 64.18, 
+    # 74.43, 50.09, 66.61, 39.06, 58.35, 46.74, 77.41, 74.75, 73]
+    # bins_requested = 5
+
+    freq, bins  = numpy.histogram(forecastData,buckets)
+    
+    binCount = (len(bins) - 1)
+    print("Min\tMax\tFreq\n")
+    for index,item in enumerate(bins, start=0):
+        if index != binCount:
+            print(item,end="\t")
+            print(bins[index+1],end="\t")
+            print(freq[index],end="")
+            print("\n")
 
 
 ################################
@@ -269,46 +266,46 @@ class SimpleTimer(object):
         self.elapsed = (self.stopTime - self.startTime)
         return self.elapsed
 
-    def printSummary(self, doingWhat="do something"):
-        print("\n")
-        print("[ {0} ] Time elapsed {1}".format(timeFromFloat(self.elapsed),doingWhat))
+    def PrintSummary(self, doingWhat="do something"):
+        return "[ {0} ] Time elapsed {1}".format(TimeFromFloat(self.elapsed),doingWhat)
 
 
-def calculatePercentage(x,y):
+def CalculatePercentage(x,y):
     p = 0
     if(x is not 0):
         p = ((x / y) * 100)
-    return ('{:.1f}%'.format(p))
+    return ('{:.2f}%'.format(p))
 
 
-def timeFromFloat(f):
+def TimeFromFloat(f):
     return time.strftime("%H:%M:%S", time.gmtime(f))
 
 
-def printProgress(total, current,currentElapsed):
-    print("->")
+def PrintProgress(total, current,currentElapsed):
+    #print("->")
     if(not current % 5):
-        printMessage(MessageType.INFO,
+        PrintMessage(MessageType.INFO,
             "Processing [{0}] of [{1}] - [{2}] percent complete - Elapsed time [{3}]"\
-            .format(current,total,calculatePercentage(current,total), timeFromFloat(currentElapsed)))
+            .format(current,total,CalculatePercentage(current,total), TimeFromFloat(currentElapsed)))
 
 
-def printMessage(messageType,friendlyMessage,detailMessage="None"):
+def PrintMessage(messageType,friendlyMessage,detailMessage="None"):
     print("[{0}] - {1} - More Details [{2}]".format(str(messageType.name),friendlyMessage,detailMessage))
 
 
-def printSummary(data,attempts,failures,description,title, debug):
+def PrintSummary(data,attempts,failures,timeSummary,description,title, debug):
         print("-----------------------------------------")
         print("- STEP SUMMARY : [{0}] ".format(title))
         if debug:
             print("- {0} data [{1}]".format(description,data))
         print("- {0} attempted [{1}]".format(description,attempts))
         print("- {0} failures [{1}]".format(description,failures))
-        print("- {0} failed: [{1}]".format(description,calculatePercentage(failures,attempts)))
+        print("- {0} failed: [{1}]".format(description,CalculatePercentage(failures,attempts)))
+        print(timeSummary)
         print("-----------------------------------------")
 
-def printFinalSummary():
-    #TODO - -
+def PrintFinalSummary():
+    #TODO -
     pass
 
 
